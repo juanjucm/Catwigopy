d752ab0a5a78c3308a375494e25088416d034ed2
Juan Julián Cea Morán <juanjucm17@gmail.com>
2018-12-04 19:27:46 +0100

    Initial packages added

---
+++ twitterpreferencesprofile/twitterretriever/secret.py
@@ -0,0 +1,7 @@
+
+# Contains the credentials for the Twitter application used to crawl.
+
+consumer_key = 'Q2OBHvyHL0jPUGfKpg2JvYO4O'
+consumer_secret = 'nR6rG8RaFxpQMQvrMUX6twhZCf6UNp4wx8MiV5okV53qgSwOp7'
+access_token = '993175870545293312-GvTQWOBF07UUKJFLPhy2l0wptkZDuA0'
+access_token_secret = 'mQ3bII85nPUjtP0FWHbR8t34ft22KY3wGtACqKfTaRuyk'
